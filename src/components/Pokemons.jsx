42da26baed096d5e32b23342074b4edbe851f5e0
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-05-24 22:42:38 -0400

    first commit no style

---
+++ src/components/Pokemons.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Pokemons() {
+    const [pokeList, setPokeList] = useState([]);
+    const [number, setNumber] = useState(5);
+
+    const handleNumber = (e) => {
+        e.perventDefault();
+        setNumber(5);
+    };
+
+    useEffect(() => {
+        axios
+            .get(`https://pokeapi.co/api/v2/pokemon?limit=${number}`)
+            // .then((res) => console.log(res)) // json structure is different from using fetch
+            .then((response) => setPokeList(response.data.results)) // <-- this part will be different from using fetch
+            .catch((err) => console.log(err));
+    }, [number]);
+
+    return (
+        <div>
+            <form onSubmit={(e) => handleNumber(e)}>
+                <label>Number of Pokemons in displat</label>
+                <input
+                    type="number"
+                    onChange={(e) => setNumber(e.target.value)}
+                    value={number}
+                />
+            </form>
+            {pokeList.map((pokeRes, index) => (
+                <p key={index}>{`Number : ${index + 1} - ${pokeRes.name}`}</p>
+            ))}
+        </div>
+    );
+}
